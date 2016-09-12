55688a67b9a32f39f6b9992d4fe987ab801bf43a
Daniel Haehn <haehn@seas.harvard.edu>
2016-09-12 13:00:44 -0400

    Add slurm and driver for RGBplus full training.

---
+++ train/rgbplus_full.py
@@ -0,0 +1,22 @@
+import cPickle as pickle
+import os; import sys; sys.path.append('..')
+import gp
+import gp.nets as nets
+
+PATCH_PATH = ('cylinder2_rgb')
+
+X_train, y_train, X_test, y_test = gp.Patch.load_rgb(PATCH_PATH)
+
+
+cnn = nets.RGBNetPlus()
+
+cnn = cnn.fit(X_train, y_train)
+
+test_accuracy = cnn.score(X_test, y_test)
+
+print test_accuracy
+
+# store CNN
+sys.setrecursionlimit(1000000000)
+with open(os.path.expanduser('~/Projects/gp/nets/RGBPlus_FULL.p'), 'wb') as f:
+  pickle.dump(cnn, f, -1)
