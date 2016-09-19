35efc2c1c415c7bd9ab1d7e4d56bbe3d03915e78
Daniel Haehn <haehn@seas.harvard.edu>
2016-09-19 15:06:21 -0400

    Ready to run all new net configurations.

--- gp/patch.py
+++ gp/patch.py
@@ -607,9 +607,13 @@ class Patch(object):
   def load_rgba(PATCH_PATH, border_prefix='border', patch_size=(75,75), verbose=True):
 
     PATCH_PATH_ = '/tmp/' + os.sep + PATCH_PATH + os.sep
-    if not os.path.exists(PATCH_PATH):
-      PATCH_PATH_ = '/n/regal/pfister_lab/haehn' + os.sep + PATCH_PATH + os.sep    
-    if not os.path.exists(PATCH_PATH):
+    if not os.path.exists(PATCH_PATH_):
+      PATCH_PATH_ = '/n/pfister_lab/haehn/patches/' + PATCH_PATH + os.sep
+    if not os.path.exists(PATCH_PATH_):
+      PATCH_PATH_ = '/n/regal/pfister_lab/haehn' + os.sep + PATCH_PATH + os.sep
+    if not os.path.exists(PATCH_PATH_):
+      PATCH_PATH_ = os.path.expanduser('~/patches_local/') + os.sep + PATCH_PATH + os.sep
+    if not os.path.exists(PATCH_PATH_):
       PATCH_PATH_ = os.path.expanduser('~/patches/') + os.sep + PATCH_PATH + os.sep
 
     PATCH_PATH = PATCH_PATH_
@@ -631,7 +635,9 @@ class Patch(object):
   def load_rgb(PATCH_PATH, patch_size=(75,75), verbose=True):
 
     PATCH_PATH_ = '/tmp/' + os.sep + PATCH_PATH + os.sep
-    if not os.path.exists(PATCH_PATH):
+    if not os.path.exists(PATCH_PATH_):
+      PATCH_PATH_ = '/n/pfister_lab/haehn/patches/' + PATCH_PATH + os.sep
+    if not os.path.exists(PATCH_PATH_):
       PATCH_PATH_ = '/n/regal/pfister_lab/haehn' + os.sep + PATCH_PATH + os.sep
     if not os.path.exists(PATCH_PATH_):
       PATCH_PATH_ = os.path.expanduser('~/patches_local/') + os.sep + PATCH_PATH + os.sep
@@ -651,7 +657,7 @@ class Patch(object):
     if verbose:
       print 'Loaded', PATCH_PATH, 'in', time.time()-t0, 'seconds.'
 
-    return training['rgb'], training_targets['targets'].astype(np.uint8), test['rgb'], test_targets['targets'].astype(np.uint8)
+    return training['rgba'], training_targets['targets'].astype(np.uint8), test['rgba'], test_targets['targets'].astype(np.uint8)
 
 
 
