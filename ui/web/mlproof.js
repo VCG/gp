2448ea05595b287588765dfbaa72086f9823dd38
Daniel Haehn <haehn@seas.harvard.edu>
2016-11-07 14:25:52 -0500

    Set timelimit to 30 minutes.

--- ui/web/mlproof.js
+++ ui/web/mlproof.js
@@ -10,7 +10,7 @@ MLProof.start = function() {
     MLProof.CORRECTIONS = [];
 
     MLProof.EXPERIMENT_START_TIME = 0;
-    MLProof.EXPERIMENT_END_TIME = 1*60*1000;
+    MLProof.EXPERIMENT_END_TIME = 30*60*1000;
 
     // onclick handler for corrections
     $('.correction').on('click', MLProof.apply_correction);
