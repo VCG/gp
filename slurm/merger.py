ca56c1ff751a1a547b62455725c09a05d612075d
Daniel Haehn <haehn@seas.harvard.edu>
2016-11-07 21:41:52 -0500

    Updates to the huge merger.

--- slurm/merger.py
+++ slurm/merger.py
@@ -12,8 +12,8 @@ train2 = basedir + 'train2b.npz'
 train2_targets = basedir + 'train2b_targets.npz'
 train3 = basedir + 'train3b.npz'
 train3_targets = basedir + 'train3b_targets.npz'
-train4 = basedir + 'train4b.npz'
-train4_targets = basedir + 'train4b_targets.npz'
+train4 = basedir + 'train4.npz'
+train4_targets = basedir + 'train4_targets.npz'
 train5 = basedir + 'train5b.npz'
 train5_targets = basedir + 'train5b_targets.npz'
 
@@ -45,10 +45,10 @@ P_SIZE = (NO_PATCHES, 4, 75,75) # rather than raveled right now
 
 p_rgba = np.zeros(P_SIZE, dtype=np.float32)
 
-p_rgba[0:train1_count] = train1
+p_rgba[0:train1_count] = train1['rgba']
 p_rgba[train1_count:train1_count+train2_count] = train2
 p_rgba[train2_count:train2_count+train3_count] = train3
-p_rgba[train3_count:train3_count+train4_count] = train4
+p_rgba[train3_count:train3_count+train4_count] = train4['rgba']
 p_rgba[train4_count:train4_count+train5_count] = train5
 
 # now store this bad boy
@@ -73,10 +73,10 @@ train5_targets = np.load(train5_targets)
 
 p_target = np.zeros(NO_PATCHES)
 
-p_target[0:train1_count] = train1_targets
+p_target[0:train1_count] = train1_targets['targets']
 p_target[train1_count:train1_count+train2_count] = train2_targets
 p_target[train2_count:train2_count+train3_count] = train3_targets
-p_target[train3_count:train3_count+train4_count] = train4_targets
+p_target[train3_count:train3_count+train4_count] = train4_targets['targets']
 p_target[train4_count:train4_count+train5_count] = train5_targets
 
 # now store this lady boy
