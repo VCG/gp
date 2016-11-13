a507b16abe1c54ebdfc478b6572e5c4acf6a8f2b
Daniel Haehn <haehn@seas.harvard.edu>
2016-11-13 11:47:03 -0500

    Also run everything with threshold=0.

--- cylinderGP.py
+++ cylinderGP.py
@@ -121,6 +121,78 @@ print '   Median VI improvement', original_median_VI-cylinder_vi_95[1]
 
 
 
+
+#
+# run auto merge correction
+#
+cylinder_vi_95_file = OUTDIR + '/cylinder_vi_00_w_merge.p'
+
+merge_vis = OUTDIR + '/cylinder_merge_auto00_vis.p'
+split_vis = OUTDIR + '/cylinder_split_auto00_vis.p'
+
+merge_fixes = OUTDIR + '/cylinder_merge_auto00_fixes.p'
+split_fixes = OUTDIR + '/cylinder_split_auto00_fixes.p'
+
+output_95 = OUTDIR + '/cylinder_auto00_output.p'
+
+if os.path.exists(cylinder_vi_95_file):
+  print 'Loading merge errors p < .05 and split errors p > .00 from file..'
+  with open(cylinder_vi_95_file, 'rb') as f:
+    cylinder_vi_95 = pickle.load(f)
+
+else:      
+  #
+  # perform merge correction with p < .05
+  #
+  print 'Correcting merge errors with p < .05'
+  bigM_05, corrected_rhoana_05, cylinder_auto_merge_fixes, vi_s_per_step = gp.Legacy.perform_auto_merge_correction(cnn, bigM, input_image, input_prob, input_rhoana, merge_errors, .05, input_gold=input_gold)
+
+  print '   Mean VI improvement', original_mean_VI-gp.Legacy.VI(input_gold, corrected_rhoana_05)[0]
+  print '   Median VI improvement', original_median_VI-gp.Legacy.VI(input_gold, corrected_rhoana_05)[1]
+
+  with open(merge_vis, 'wb') as f:
+    pickle.dump(vi_s_per_step, f)
+
+
+  with open(merge_fixes, 'wb') as f:
+    pickle.dump(cylinder_auto_merge_fixes, f) 
+
+
+#
+# run auto split correction
+#
+  print 'Correcting split errors with p > .0'
+  bigM_cylinder_after_95, out_cylinder_volume_after_auto_95, cylinder_auto_fixes_95, cylinder_auto_vi_s_95, vi_s_per_step2 = gp.Legacy.splits_global_from_M_automatic(cnn, bigM_05, input_image, input_prob, corrected_rhoana_05, input_gold, sureness_threshold=.00)
+
+  cylinder_vi_95 = gp.Legacy.VI(input_gold, out_cylinder_volume_after_auto_95)
+
+  with open(cylinder_vi_95_file, 'wb') as f:
+    pickle.dump(cylinder_vi_95, f)
+
+  with open(split_vis, 'wb') as f:
+    pickle.dump(vi_s_per_step2, f)
+
+  with open(split_fixes, 'wb') as f:
+    pickle.dump(cylinder_auto_fixes_95, f)       
+
+  with open(output_95, 'wb') as f:
+    pickle.dump(out_cylinder_volume_after_auto_95, f) 
+
+
+print '   Mean VI improvement', original_mean_VI-cylinder_vi_95[0]
+print '   Median VI improvement', original_median_VI-cylinder_vi_95[1]
+
+
+
+
+
+
+
+
+
+
+
+
 #
 # run oracle merge correction
 #
